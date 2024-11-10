c898978ef9801f7540416ee6241c4731b5041f8f
FFooCuuSS <106500256+FFooCuuSS@users.noreply.github.com>
2024-11-10 17:54:11 +0900

    플레이어 공격 완성
    본 리포지토리 올려주세요

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
+    public GameObject player;
+    public GameObject strike1;
+    public GameObject strike2;
+    public GameObject strike3;
+
     private int whatSwordAttack = 0;
+    private SwordStrike swordScript;
+    private Vector3 strikeDirection;
     public void doSwordAttack()
     {
         if (whatSwordAttack == 0)
@@ -40,13 +47,47 @@ public class Sword : MonoBehaviour
     private void firstSword()
     {
         Debug.Log("첫번째");
+        GameObject a = Instantiate(strike1, transform.position, transform.rotation);
+        Destroy(a, 1f);
+        if (player.transform.position.x < this.transform.position.x)
+        {
+            player.transform.position += transform.right * 1f;
+            a.transform.rotation = Quaternion.Euler(0, 0, -90f);
+        }
+        else
+        {
+            player.transform.position += -transform.right * 1f;
+            a.transform.rotation = Quaternion.Euler(0, 0, 90f);
+        }
     }
     private void secondSword()
     {
         Debug.Log("두번째");
+        
+        if (player.transform.position.x < this.transform.position.x)
+        {
+            this.transform.position += transform.right * 3f;
+            GameObject b = Instantiate(strike2, transform.position, transform.rotation);
+            Destroy(b, 1f);
+            this.transform.position -= transform.right * 3f;
+            player.transform.position += transform.right * 3f;
+            b.transform.rotation = Quaternion.Euler(0, 0, -90f);
+        }
+        else
+        {
+            this.transform.position -= transform.right * 3f;
+            GameObject b = Instantiate(strike2, transform.position, transform.rotation);
+            Destroy(b, 1f);
+            this.transform.position += transform.right * 3f;
+            player.transform.position += -transform.right * 3f;
+            b.transform.rotation = Quaternion.Euler(0, 0, 90f);
+        }
     }
     private void thirdSword()
     {
         Debug.Log("세번째");
+        GameObject c = Instantiate(strike3, transform.position, transform.rotation);
+        Destroy(c, 1f);
+        player.transform.position += transform.up * 2f;
     }
 }
