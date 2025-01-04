0d6d4f69bdbc63bfb747a91f212f924290ff8aa8
FFooCuuSS <106500256+FFooCuuSS@users.noreply.github.com>
2025-01-04 13:11:51 +0900

    세이브 로드창 제작
    로드창 ui 제작 및 세이브 포인트 제작

--- Assets/Scripts/SaveLoad.cs
+++ Assets/Scripts/SaveLoad.cs
@@ -2,29 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SaveLoad : MonoBehaviour
 {
     public Button[] saveSlots; // 10개의 슬롯 버튼 배열
-    public static int savePoint; // 세이브 포인트 수치 (게임플레이에서 증가)
+    public static int savePoint = 5; // 세이브 포인트 수치 (게임플레이에서 증가)
     public static int currentSavePoint;
     public GameObject slotPrefab;
+    public Transform SaveSlotCanvas;
 
     float[,] uiPos = {
-        { -7f,  1.6f },
-        { -3.5f, 1.6f },
-        { 0f, 1.6f },
-        { 3.5f,  1.6f },
-        { 7f, 1.6f },
-        { -7f,  -1.6f },
-        { -3.5f, -1.6f },
-        { 0f, -1.6f },
-        { 3.5f,  -1.6f },
-        { 7f, -1.6f }
+        { -400f,  100f },
+        { -200f, 100f },
+        { 0f, 100f },
+        { 200f,  100f },
+        { 400f, 100f },
+        { -400f,  -100f },
+        { -200f, -100f },
+        { 0f, -100f },
+        { 200f,  -100f },
+        { 400f, -100f }
     };
     void Start()
     {
         UpdateSaveSlots();
+        GenerateSaveSlots(10);
     }
 
     // 세이브 슬롯 활성화/비활성화 상태 업데이트
@@ -51,7 +54,8 @@ public class SaveLoad : MonoBehaviour
             Vector3 uiPosition = new Vector3(uiPos[i,0], uiPos[i, 1], 0f);
             GameObject newSlot = Instantiate(slotPrefab, uiPosition, transform.rotation); // 슬롯 생성
             saveSlots[i] = newSlot.GetComponent<Button>();
-
+            newSlot.transform.SetParent(SaveSlotCanvas.transform, false); // 캔버스의 Transform 지정
+            newSlot.GetComponent<RectTransform>().anchoredPosition = new Vector2(uiPos[i, 0], uiPos[i, 1]);
             // 슬롯의 버튼 클릭 이벤트 설정
             int slotIndex = i; // 클로저 문제 방지
             saveSlots[i].onClick.AddListener(() => OnSlotClicked(slotIndex));
@@ -64,7 +68,7 @@ public class SaveLoad : MonoBehaviour
         if (slotIndex < savePoint)
         {
             Debug.Log($"Slot {slotIndex + 1} 선택됨. 세이브 데이터를 로드합니다.");
-            // 여기에 로드 로직 추가
+            SceneManager.LoadScene("test");
         }
         else
         {
