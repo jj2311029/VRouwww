52f89804cfbe0faae46166a7943e7eab31e4cb20
jj2311029 <151961300+jj2311029@users.noreply.github.com>
2025-02-06 22:54:58 +0900

    해적선 맵 완성

--- Assets/Scripts/PirateShip/SpawnObject.cs
+++ Assets/Scripts/PirateShip/SpawnObject.cs
@@ -24,7 +24,7 @@ public class SpawnObject : MonoBehaviour
         while (true)
         {
             Vector3 randomPos=new(screenWidth,Random.Range(-screenHeight, screenHeight),0);
-            Instantiate(Item, randomPos,Quaternion.identity);
+            Instantiate(Item, randomPos,Quaternion.identity,transform);
             yield return new WaitForSeconds(coolTime);
         }
     }
